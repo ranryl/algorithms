83bb02b50e0d0aacf1150a58e3929f3c1c1593e7
ranrl <ranrl@qq.com>
2024-01-10 10:21:00 +0800

    Update bloom.go

--- bloom/bloom.go
+++ bloom/bloom.go
@@ -1,1 +1,35 @@
 package bloom
+
+import "mybloomfilter/bitmap"
+
+type Bloom struct {
+	bitMap   bitmap.BitMap
+	hashList []BloomFilterHash
+}
+
+type BloomFilterHash func(data []byte) uint64
+
+func NewBloom(lens uint64, hashFuncs []BloomFilterHash) *Bloom {
+	return &Bloom{
+		bitMap:   *bitmap.NewBitMap(lens),
+		hashList: hashFuncs,
+	}
+}
+
+func (b *Bloom) Add(data []byte) {
+	bitMapLens := b.bitMap.Len()
+	for _, v := range b.hashList {
+		hashValue := v(data) % bitMapLens
+		b.bitMap.Set(hashValue)
+	}
+}
+func (b *Bloom) IsExsit(data []byte) bool {
+	bitMapLens := b.bitMap.Len()
+	for _, v := range b.hashList {
+		hashValue := v(data) % bitMapLens
+		if !b.bitMap.IsExist(hashValue) {
+			return false
+		}
+	}
+	return true
+}
